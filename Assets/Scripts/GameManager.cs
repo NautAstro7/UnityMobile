2c3d06c896a7ac1404dd5f2db0283f4eac4b0bfb
Naut <vinicius.costa62@fatec.sp.gov.br>
2025-09-08 16:38:28 -0300

    Aula 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic; // List
 
 /// <summary>
 /// Manages the main gameplay of the game
@@ -9,12 +10,18 @@ public class GameManager : MonoBehaviour{
     [Tooltip("A reference to the tile we want to spawn")]
     public Transform tile;
 
+    [Tooltip("A reference to the obstacle we want to spawn")]
+    public Transform obstacle;
+
     [Tooltip("Where the first tile should be placed at")]
     public Vector3 startPoint = new Vector3(0,0, -5);
 
     [Tooltip("How many tiles should we create in advance")]
     [Range(1,15)]
     public int initSpawnNum = 10;
+
+    [Tooltip("How many tiles should we create without obstacles")]
+    public int initNoObstacles = 10;
 
     [Tooltip("Where the nex tile should be spawned at")]
     private Vector3 nextTileLocation;
@@ -29,12 +36,12 @@ public class GameManager : MonoBehaviour{
         nextTileRotation = Quaternion.identity;
 
         for (int i = 0; i < initSpawnNum; ++i){
-            SpawnNextTile();
+            SpawnNextTile(i >= initNoObstacles);
         }
 
     }
 
-    public void SpawnNextTile(){
+    public void SpawnNextTile(bool spawnObstacles = true){
         
         var newTile = Instantiate(tile, 
         nextTileLocation, nextTileRotation);
@@ -42,6 +49,39 @@ public class GameManager : MonoBehaviour{
         var nextTile = newTile.Find("NextSpawnPoint");
         nextTileLocation = nextTile.position;
         nextTileRotation = nextTile.rotation;
+
+        if(spawnObstacles){
+            SpawnObstacle(newTile);
+        }
+
+    }
+
+
+    public void SpawnObstacle(Transform newTile){
+
+        var obsSpawnPoints = new List<GameObject>();
+
+        foreach (Transform child in newTile){
+
+            if (child.CompareTag("ObstacleSpawn")){
+                obsSpawnPoints.Add(child.gameObject);
+            }
+
+        }
+
+        if(obsSpawnPoints.Count > 0){
+
+            int index = Random.Range(0, obsSpawnPoints.Count);
+
+            var spawnPoint = obsSpawnPoints[index];
+
+            var spawnPos = spawnPoint.transform.position;
+
+            var newObstacle = Instantiate(obstacle, spawnPos, Quaternion.identity);
+
+            newObstacle.SetParent(spawnPoint.transform);
+
+        }
 
     }
 
